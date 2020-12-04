3e81d097cd2ae48be7ce24bbdf84246c83ebf01e
Hyun-jin Won <ohj8447@gmail.com>
2020-12-04 16:17:14 +0900

    add : add login Page

--- src/main/java/com/mumuni/toy/springboot/web/IndexController.java
+++ src/main/java/com/mumuni/toy/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.mumuni.toy.springboot.web;
 
+import com.mumuni.toy.springboot.config.auth.LoginUser;
+import com.mumuni.toy.springboot.config.auth.dto.SessionUser;
 import com.mumuni.toy.springboot.domain.posts.PostsService;
 import com.mumuni.toy.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index"; //gradle 'org.springframework.boot:spring-boot-starter-mustache' 로 인해 앞의 경로와 파일확장자가 자동 입력됨
                         //해당 경우 default 경로로서 src/main/resource/templates/index.mustache 로 전환되어 View Resolver 가 처리하게 됨
     }
@@ -32,5 +42,4 @@ public class IndexController {
 
         return "post-update";
     }
-
 }
